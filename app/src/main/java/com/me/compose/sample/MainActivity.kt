03340812edc7ce19ab166f77cd4d12cd90447d84
Atthaphon <>
2022-06-03 22:06:01 +0700

    Summary Define composable functions. Create layouts with Compositions. Create buttons with Button composables. Import drawable resources. Display images with Image composables. Make an interactive UI with composables. Use remember composable to store objects in a Composition to memory. Refresh the UI with the mutableStateOf()function to make an observable.

--- app/src/main/java/com/me/compose/sample/MainActivity.kt
+++ app/src/main/java/com/me/compose/sample/MainActivity.kt
@@ -7,12 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -26,149 +27,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column(
-                        Modifier
-                            .fillMaxSize()
-                    ) {
-                        Column(
-                            modifier = Modifier
-                                .weight(4f)
-                                .wrapContentHeight(Alignment.CenterVertically)
-                        ) {
-                            CardHeader()
-                        }
-                        Column(modifier = Modifier.weight(1f)) {
-                            CardDescription(iconRes = R.drawable.ic_info_black_24dp, "info")
-                            CardDescription(
-                                iconRes = R.drawable.ic_check_circle_black_24dp,
-                                "check"
-                            )
-                            CardDescription(iconRes = R.drawable.ic_delete_black_24dp, "delete")
-                        }
-
-                    }
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
+@Preview()
 @Composable
-fun Greeting(name: String, from: String) {
-
-    Box(content = {
-        Column(
-            modifier = Modifier.fillMaxHeight(),
-            content = {
-                Text(
-                    text = "Hello $name",
-                    fontSize = 32.sp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                        .padding(10.dp)
-                )
-                Text(
-                    text = "From $from", modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                        .padding(10.dp)
-                )
-            })
-    })
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
 
+@Preview(showBackground = true)
 @Composable
-fun CardDescription(iconRes: Int, message: String) {
-    val iconPainter = painterResource(id = iconRes)
-    Column {
-        Spacer(modifier = Modifier.height(14.dp))
-        Row {
-            Spacer(modifier = Modifier.width(50.dp))
-            Icon(painter = iconPainter, contentDescription = null, tint = Color.White)
-            Spacer(modifier = Modifier.width(50.dp))
-            Text(
-                text = message,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentWidth(Alignment.CenterHorizontally)
-                    .wrapContentHeight(Alignment.CenterVertically),
-                fontSize = 18.sp,
-                style = TextStyle(color = Color.White)
-            )
-        }
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
-}
-
-@Composable
-fun CardHeader() {
-    val image = painterResource(R.drawable.ic_launcher_foreground)
-    Column(
-    ) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
-            painter = image, contentDescription = "image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth()
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
         )
-        Text(
-            text = "Att Mui",
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally),
-            fontSize = 38.sp
-        )
-        Text(
-            text = "Att Mui",
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally),
-            fontSize = 12.sp,
-            style = TextStyle(color = Color.Green, fontWeight = FontWeight.Bold)
-        )
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    AndroidcomposeTheme {
-        Column(
-            Modifier
-                .fillMaxSize()
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(4f)
-                    .wrapContentHeight(Alignment.CenterVertically)
-            ) {
-                CardHeader()
-            }
-            Column(modifier = Modifier.weight(1f)) {
-                CardDescription(iconRes = R.drawable.ic_info_black_24dp, "info")
-                CardDescription(iconRes = R.drawable.ic_check_circle_black_24dp, "check")
-                CardDescription(iconRes = R.drawable.ic_delete_black_24dp, "delete")
-            }
-
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(R.string.roll))
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun CardHeaderPreview() {
-    CardHeader()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CardDescriptionPreview() {
-    CardDescription(iconRes = R.drawable.ic_info_black_24dp, "demo")
-}
-
